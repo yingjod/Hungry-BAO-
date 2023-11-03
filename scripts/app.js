90a2843a350bf4d4d9b9525c76db65a6f79527ce
Ying L <forgmina@gmail.com>
2023-11-03 16:21:46 +0000

    add folder and sudecode

---
+++ scripts/app.js
@@ -0,0 +1,48 @@
+// *Elements
+// current-level
+
+// lives-amount
+// current-score
+// current-highest-score
+// start-button (for start the game)
+
+
+// *Variables
+//currentLevel (update when the level up)
+//livesAmount (will decrease when the Bao be hit)
+//currentScore (will increse when the Bao cross each row)
+//current-highest-score (will update when the player hit the current highest score)
+
+//*Excutions
+
+//*start the game
+// The function that executes when a start button is clicked
+// the items moveing in the rows every 2 seconds
+// game music start playing
+
+//*Increase the current-score
+// update the score when Bao success cross every singel row
+
+//*Update the current-highest-score
+// check the current-score is over the current-highest-score
+// if it is , update the current-highest-score
+
+//*Increase the current-level
+//when the Bao reach to the steamer update the currentLevel
+//the items start to move in the rows every 1.5 seconds
+
+//* decrease the lives-amount 
+//check for collision
+//collision = true  will lose a life and game restart
+
+//* end the game
+//when the lives-amount equel to 0, game finish.
+//all the items dispear on the grid
+//alarm on the page, showing final score
+//game music stop playing
+
+
+//*Event Listeners
+//click event triggering on start-button
+//Keypress event 
+
